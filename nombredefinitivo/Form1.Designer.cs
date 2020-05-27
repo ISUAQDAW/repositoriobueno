7979768798c0fe0052e6e09a761e295896e8e775
ISUAQDAW <ulalam17246@iesmarenostrum.com>
2020-05-28 01:44:50 +0200

    cambios

--- nombredefinitivo/Form1.Designer.cs
+++ nombredefinitivo/Form1.Designer.cs
@@ -89,7 +89,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(600, 77);
+            this.button1.Location = new System.Drawing.Point(612, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
