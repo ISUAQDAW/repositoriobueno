1879f58cef16e4cc5ea99d75d91677f308ff6c72
ulalam17246@iesmarenostrum.com <ulalam17246@iesmarenostrum.com>
2020-05-28 02:14:50 +0200

    cambios

--- nombredefinitivo/Form1.Designer.cs
+++ nombredefinitivo/Form1.Designer.cs
@@ -42,83 +42,93 @@
             // Nombre
             // 
             this.Nombre.AutoSize = true;
-            this.Nombre.Location = new System.Drawing.Point(32, 41);
+            this.Nombre.Location = new System.Drawing.Point(48, 63);
+            this.Nombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Nombre.Name = "Nombre";
-            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.Size = new System.Drawing.Size(65, 20);
             this.Nombre.TabIndex = 0;
             this.Nombre.Text = "Nombre";
             // 
             // tNombre
             // 
-            this.tNombre.Location = new System.Drawing.Point(99, 34);
+            this.tNombre.Location = new System.Drawing.Point(148, 52);
+            this.tNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tNombre.Name = "tNombre";
-            this.tNombre.Size = new System.Drawing.Size(100, 20);
+            this.tNombre.Size = new System.Drawing.Size(148, 26);
             this.tNombre.TabIndex = 1;
             // 
             // tApellidos
             // 
-            this.tApellidos.Location = new System.Drawing.Point(323, 34);
+            this.tApellidos.Location = new System.Drawing.Point(484, 52);
+            this.tApellidos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tApellidos.Name = "tApellidos";
-            this.tApellidos.Size = new System.Drawing.Size(100, 20);
+            this.tApellidos.Size = new System.Drawing.Size(148, 26);
             this.tApellidos.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(244, 37);
+            this.label2.Location = new System.Drawing.Point(366, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(73, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Apellidos";
             // 
             // tTelefono
             // 
-            this.tTelefono.Location = new System.Drawing.Point(575, 34);
+            this.tTelefono.Location = new System.Drawing.Point(862, 52);
+            this.tTelefono.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tTelefono.Name = "tTelefono";
-            this.tTelefono.Size = new System.Drawing.Size(100, 20);
+            this.tTelefono.Size = new System.Drawing.Size(148, 26);
             this.tTelefono.TabIndex = 4;
             // 
             // Teléfono
             // 
             this.Teléfono.AutoSize = true;
-            this.Teléfono.Location = new System.Drawing.Point(496, 37);
+            this.Teléfono.Location = new System.Drawing.Point(744, 57);
+            this.Teléfono.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Teléfono.Name = "Teléfono";
-            this.Teléfono.Size = new System.Drawing.Size(49, 13);
+            this.Teléfono.Size = new System.Drawing.Size(71, 20);
             this.Teléfono.TabIndex = 5;
             this.Teléfono.Text = "Teléfono";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(616, 77);
+            this.button1.Location = new System.Drawing.Point(924, 118);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 6;
             this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 82);
+            this.label1.Location = new System.Drawing.Point(69, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.Size = new System.Drawing.Size(95, 20);
             this.label1.TabIndex = 7;
             this.label1.Text = "Lista Socios";
             // 
             // tListaSocios
             // 
-            this.tListaSocios.Location = new System.Drawing.Point(35, 131);
+            this.tListaSocios.Location = new System.Drawing.Point(52, 202);
+            this.tListaSocios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tListaSocios.Multiline = true;
             this.tListaSocios.Name = "tListaSocios";
             this.tListaSocios.ReadOnly = true;
-            this.tListaSocios.Size = new System.Drawing.Size(724, 260);
+            this.tListaSocios.Size = new System.Drawing.Size(1084, 398);
             this.tListaSocios.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.tListaSocios);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -128,6 +138,7 @@
             this.Controls.Add(this.tApellidos);
             this.Controls.Add(this.tNombre);
             this.Controls.Add(this.Nombre);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
